8a5a4c801adab7075737ba1b2630fa8837d8e5cb
PDice30 <padeisinger@gmail.com>
2023-07-25 19:50:30 -0500

    styling updates

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -10,7 +10,6 @@ const Overlay = () => {
   const [storedId, setStoredId] = useState<number>(0);
 
   useEffect(() => {    
-    console.log(import.meta.env);
     // TODO : react-query
     // If Winners.length === 8: Tournament is over, display "waiting" and wait a set time to call again and show teams
     const fetchData = async () => {
@@ -21,6 +20,7 @@ const Overlay = () => {
 
       // TODO: move to helper function
       const id = (await (await fetch('https://fftbg.com/api/tournaments?limit=1')).json() as Tournament[])[0].ID.toString();
+      console.log(id);
       setStoredId(parseInt(id));
       // TODO : Fix linting
       const tournament = (await (await fetch(`https://fftbg.com/api/tournament/${id}`)).json() as Tournament);
@@ -49,16 +49,17 @@ const Overlay = () => {
 
   // TODO - teams array could just be one to iterate through, and first half float left
   return (
-    <div className='w-4/5 h-screen' style={{fontFamily: 'Altima', fontSize: '30px'}}>
+    // <div className='w-4/5 h-screen' style={{fontFamily: 'Altima', fontSize: '30px'}}>
+    <div className={tailwindClasses.overlay} style={{fontFamily: 'Altima', fontSize: '30px'}}>
       {/* Will Need to find active teams */}
-      <div className='grid grid-cols-1 h-screen w-1/9 float-left'>
+      <div className={tailwindClasses.panelLeft}>
         {teamOne && teamOne.Units.map(unit => {
           return (
             <Portrait key={unit.Name} unit={unit} side={1}/>
           )
         })}
       </div>
-      <div className='grid grid-cols-1 h-screen w-1/9 float-right'>
+      <div className={tailwindClasses.panelRight}>
         {teamTwo && teamTwo.Units.map(unit => {
           return (
             <Portrait key={unit.Name} unit={unit} side={2}/>
@@ -68,4 +69,16 @@ const Overlay = () => {
     </div>
   )
 }
+
+const overlay = 'w-4/5 h-screen';
+const panelLeft = 'grid grid-cols-1 h-screen w-1/9 float-left';
+const panelRight = 'grid grid-cols-1 h-screen w-1/9 float-right';
+
+const tailwindClasses =  {
+  overlay: import.meta.env.PROD ? overlay : overlay + ' bg-red-100',
+  panelLeft: import.meta.env.PROD ? panelLeft : panelLeft + ' bg-blue-400',
+  panelRight: import.meta.env.PROD ? panelRight : panelRight + ' bg-blue-400',
+
+}
+
 export default Overlay;
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    cors: true,
-    port: 8080
+    cors: true
   }
 })
