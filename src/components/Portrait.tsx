c122b11bafde307a21eb7442e6e56157c42ec812
PDice30 <padeisinger@gmail.com>
2023-07-31 15:47:40 -0500

    removing context

--- src/components/Portrait.tsx
+++ src/components/Portrait.tsx
@@ -1,7 +1,6 @@
-import { useContext, useState } from "react"
+import { useState } from "react"
 import { Unit } from "../models"
 import { UnitPanel } from "./UnitPanel"
-import { UnitContext } from "../contexts/UnitContext"
 
 type PortraitProps = {
   key: string,
@@ -18,25 +17,14 @@ type PortraitProps = {
 export const Portrait = (props: PortraitProps) => {
   const [isShown, setIsShown] = useState(false);
   const unit = props.unit;
-  let unitContext = useContext(UnitContext);
-
-  // TODO : unit Context on hover
-  const setUnitContext = (unit: Unit | undefined) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    unitContext = unit;
-
-    console.log(unitContext);
-  }
 
   return (
     <div
       onMouseEnter={() => {
         setIsShown(true);
-        setUnitContext(props.unit);
       }}
       onMouseLeave={() => {
         setIsShown(false);
-        setUnitContext(undefined);
       }}>
         <div>
           { isShown && 
--- src/contexts/UnitContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { Unit } from "../models";
-
-// TODO: find proper typing for this, or set a default context
-export const UnitContext = createContext<Unit | undefined>(undefined);
