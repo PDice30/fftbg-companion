acfecd024f1cebbae2f90a70ae36a3270a84970b
PDice30 <padeisinger@gmail.com>
2023-08-08 10:53:50 -0500

    cleanup

--- src/utils/dataHelper.ts
+++ src/utils/dataHelper.ts
@@ -1,12 +1,10 @@
 import { BASE_API_URL } from "../constants";
 import { Team, Tournament } from "../models";
-import defaultTournament from "data/tournament";
-
-// import fetchJSON from "./fetch";
+import defaultTournament from "../data/tournament";
 import determineMatchup from "./matchup";
+// import fetchJSON from "./fetch";
 
 // TODO: Once id is gotten and tournament is retrieved, only need first call to determine winners
-
 const getData = async (): Promise<[Team, Team]> => {
   // const id = (await (await fetchJSON('https://fftbg.com/api/tournaments?limit=1')).json() as Tournament[])[0].ID.toString();
   try {
