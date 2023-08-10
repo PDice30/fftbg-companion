5edc41e2356125735b7118403827f3bd60e7f9ff
PDice30 <padeisinger@gmail.com>
2023-08-10 13:32:44 -0500

    todos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { useWindowSize } from 'usehooks-ts';
 
 // TODO: Dagger and Swords and possibly other items have distorted images
 // TODO: Better overall error handling
+// TODO: Bows or a unit with "Two Hands" support should show a two handed equipment slot icon
+// TODO: The Examine details portion should have a scrollable view for long descriptions.
 
 const App = () => {
   const { width, height } = useWindowSize();
