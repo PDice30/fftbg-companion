c2387faa36a5b27576d1a961b526bd4d5403cb35
PDice30 <padeisinger@gmail.com>
2023-09-17 17:54:26 -0500

    Fix for star sizes

--- src/components/Zodiac.tsx
+++ src/components/Zodiac.tsx
@@ -21,15 +21,18 @@ export const Zodiac = (props: ZodiacProps) => {
   const teamOne = currentUnits.slice(0, 4);
   const teamTwo = currentUnits.slice(4, 8);
 
-  const thisUnit = currentUnits[props.unitIndex]; 
+  const thisUnit = currentUnits[props.unitIndex];
+  const side = props.unitIndex <= 3 ? 0 : 1; 
 
   return (
     <>
       <div className='h-full w-1/4 float-left'>
-        <Icon style='pl-1 pb-2' name={'nothing'} type='Base' />
+        {/* Left-sided Icons */}
+        <Icon style='mb-2' name={'nothing'} type='Base' />
         {
           teamOne && teamOne.map((unit, index) => {
-            const style = index === teamOne.length - 1 ? 'pl-1' : 'pl-1 pb-1';
+            let style = index === teamOne.length - 1 ? '' : 'mb-1';
+            style += side === 0 ? ' w-full' : ' ml-2 w-full';
             return (
               <Icon style={style} key={index} name={`${compareZodiac(thisUnit.Sign, thisUnit.Gender, unit.Sign, unit.Gender)}star`} type='Zodiac' />
             );
@@ -37,10 +40,12 @@ export const Zodiac = (props: ZodiacProps) => {
         } 
       </div>
       <div className='h-full w-1/4 float-right'>
-        <Icon style='pr-1 pb-2' name={'nothing'} type='Base' />
+        {/* Right-sided Icons */}
+        <Icon style='mb-2' name={'nothing'} type='Base' />
         {
           teamTwo && teamTwo.map((unit, index) => {
-            const style = index === teamOne.length - 1 ? 'pr-1' : 'pr-1 pb-1';
+            let style = index === teamOne.length - 1 ? '' : 'mb-1';
+            style += side === 0 ? ' -ml-2 w-full' : ' w-full';
             return (
               <Icon style={style} key={index} name={`${compareZodiac(thisUnit.Sign, thisUnit.Gender, unit.Sign, unit.Gender)}star`} type='Zodiac' />
             );
