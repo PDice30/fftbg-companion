2667ba06763b9f5d254833b445e827933e5b5748
PDice30 <padeisinger@gmail.com>
2023-10-03 21:03:20 -0500

    tooltips on class and zodiac

--- src/components/panel/Details.tsx
+++ src/components/panel/Details.tsx
@@ -1,9 +1,10 @@
 import { useContext } from "react";
 import { OverlayContext } from "../../contexts/OverlayContext";
 import { getSizeUp } from "../../utils/textHelper";
+import { Tooltip } from "@mui/material";
 
 export const Details = () => {
-  const { unit, textSize } = useContext(OverlayContext);
+  const { unit, toolTips, textSize } = useContext(OverlayContext);
 
   let name = unit.Name ? unit.Name.slice(0, 12) : 'Fighter';
   let nameStyle = 'h-1/6 ' + getSizeUp(textSize);
@@ -13,6 +14,8 @@ export const Details = () => {
     nameStyle += ' font-extrabold';
   }
 
+  const classTooltip = unit.Gender === 'Monster' ? unit.Class : unit.Class + ' ' + unit.Gender;
+
   // TODO: More interesting details panel
   return (
     <div className={'h-full w-3/16 float-left px-2 ' + textSize}>
@@ -20,8 +23,12 @@ export const Details = () => {
       <div className='h-1/6'>{unit.Gender}</div>
       <div className='h-1/6'>Brave: {unit.Brave}</div>
       <div className='h-1/6'>Faith: {unit.Faith}</div>
-      <div className='h-1/6'>{unit.Class}</div>
-      <div className='h-1/6'>{unit.Sign}</div>
+      <Tooltip title={toolTips['Class']?.get(classTooltip)} arrow placement='right'>
+        <div className='h-1/6'>{unit.Class}</div>
+      </Tooltip>
+      <Tooltip title={toolTips['Zodiac']?.get(unit.Sign)} arrow placement='right'>
+        <div className='h-1/6'>{unit.Sign}</div>
+      </Tooltip>
     </div>
   )
 }
